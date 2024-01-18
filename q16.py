1a41e62b8aa608bb6cf13adfc5ce62c9a5574f7d
JYOTI-MARKUN <jyotimarkun1997@gmail.com>
2024-01-18 17:33:54 +0530

    new question

--- q16.py
+++ q16.py
@@ -5,3 +5,18 @@
     #   >= 30                             < 90                 Hot
     #   <30                                >= 90               Cool and Humid
     #   <30                                 <90                 Cool
+def weather(temperature,humidity):
+    if temperature >= 30 and humidity >= 90:
+        return "Hot and Humid"
+    elif temperature >= 30 and humidity < 90:
+        return "Hot"
+    elif temperature < 30 and humidity >= 90:
+        return "Cool and Humid"
+    elif temperature < 30 and humidity < 90:    
+        return "Cool"
+
+temperature = int(input("Enter the tempreture: "))
+humidity = int(input("Enter the humidity: "))
+
+weather_report = weather(temperature, humidity)
+print("Weather is:",weather_report)
